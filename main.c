26779bda9f2cf40c5b5e16bb4a145667da3e98d5
Timo Eberl <timo@timoeberl.de>
2026-04-20 02:25:33 +0200

    Add favicon

--- main.c
+++ main.c
@@ -661,6 +661,6 @@ sapp_desc sokol_main(int argc, char* argv[]) {
 		.width = 1280,
 		.height = 720,
 		.window_title = "Tics Tower Builder",
-		.icon.sokol_default = true
+		.icon.sokol_default = false
 	};
 }
